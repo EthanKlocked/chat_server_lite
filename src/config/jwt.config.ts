d8eb1842a793585902818b6dfdaa94d89d3ab41a
ethanklocked <ethanklocked@gmail.com>
2024-11-13 13:30:34 +0900

    refresh

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -3,10 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 export const jwtConfig = {
 	imports: [ConfigModule],
 	inject: [ConfigService],
-	useFactory: async (configService: ConfigService) => {
-		console.log(configService.get('JWT_SECRET'));
-		return {
-			secret: configService.get('JWT_SECRET')
-		};
-	}
+	useFactory: async (configService: ConfigService) => ({
+		secret: configService.get('JWT_SECRET')
+	})
 };
